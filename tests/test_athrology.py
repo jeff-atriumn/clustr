4090d4e928112fbe1b4de4765096b744514d6147
Jeff Rogers <jrogers@pinger.com>
2023-04-27 17:27:55 -0500

    adding no joint test

--- tests/test_athrology.py
+++ tests/test_athrology.py
@@ -14,6 +14,19 @@ def test_athrology_data():
     # Validate the data against the schema
     jsonschema.validate(data, schema)
 
+def test_athrology_no_joints():
+    # Load the JSON schema
+    with open('../schemas/athrology.schema.json', 'r') as f:
+        schema = json.load(f)
+
+    # Load the JSON data
+    with open('./inputs/athrology/athrology-no-joints.json', 'r') as f:
+        data = json.load(f)
+
+    with pytest.raises(jsonschema.ValidationError) as exc_info:
+        jsonschema.validate(instance=data, schema=schema)
+    assert "None is not of type 'array'" in str(exc_info.value)
+
 def test_athrology_null_joint():
     # Load the JSON schema
     with open('../schemas/athrology.schema.json', 'r') as f:

48f80f952ded2c9daf53af85012c24f0c6049928
Jeff Rogers <jrogers@pinger.com>
2023-04-27 17:10:17 -0500

    adding tests for athrology data

---
+++ tests/test_athrology.py
@@ -0,0 +1,28 @@
+import json
+import jsonschema
+import pytest
+
+def test_athrology_data():
+    # Load the JSON schema
+    with open('../schemas/athrology.schema.json', 'r') as f:
+        schema = json.load(f)
+
+    # Load the JSON data
+    with open('./inputs/athrology/athrology-data.json', 'r') as f:
+        data = json.load(f)
+
+    # Validate the data against the schema
+    jsonschema.validate(data, schema)
+
+def test_athrology_null_joint():
+    # Load the JSON schema
+    with open('../schemas/athrology.schema.json', 'r') as f:
+        schema = json.load(f)
+
+    # Load the JSON data
+    with open('./inputs/athrology/athrology-null-joint.json', 'r') as f:
+        data = json.load(f)
+
+    with pytest.raises(jsonschema.ValidationError) as exc_info:
+        jsonschema.validate(instance=data, schema=schema)
+    assert "None is not of type 'string'" in str(exc_info.value)
